824021f8c5d5dec6a6e38ed7eba9e63ca413faae
MarcoMai <marco.maizener@gmail.com>
2022-06-19 19:20:16 +1000

    structure good

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,21 @@
+# from venv import create
 from flask import Flask
+# import config
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from config import Config
+from flask_login import LoginManager
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_security import Security, login_required, SQLAlchemySessionUserDatastore
 
 
 db = SQLAlchemy()
 migrate = Migrate()
+login = LoginManager()
+login.login_view = 'login.login'
+
+admin = Admin()
 
 def create_app():
     app = Flask(__name__)
@@ -14,9 +24,22 @@ def create_app():
     with app.app_context():
         db.init_app(app)
         migrate.init_app(app, db)
+        login.init_app(app)
+        admin.init_app(app)
+
+        from app.models import User
+        @login.user_loader
+        def load_user(id):
+            return User.query.get(id)
+
+        db.create_all()
+        admin.add_view(ModelView(User, db.session))
 
     from app.routes.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from app.routes.login import login_bp as login_blueprint
+    app.register_blueprint(login_blueprint)
 
     return app
 
---
+++ app/forms.py
@@ -0,0 +1,33 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    firstname = StringField('First name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email_address=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
